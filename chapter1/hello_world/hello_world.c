fdb5194a9e9539edc4b1f5930442c38fb458b7b4
David Boh <herrboh@gmail.com>
2023-04-16 19:03:42 -0500

    first commit

---
+++ chapter1/hello_world/hello_world.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+int main(void){
+	puts("Hello, world!");
+	return EXIT_SUCCESS;
+}
+
+//
+correct safe way to do it
+*/
+
+// First example, the C programming language book. 
+//
+//
+#include <stdio.h>
+
+int main(){
+    printf("hello, world!\n");
+}
